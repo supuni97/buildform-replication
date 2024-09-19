77347e89e0bfaa4809c5ed332afe7b3898658e4a
Supuni Sithara <sitharabandara1997@gmail.com>
2024-09-19 12:42:09 +0530

    Added basic fields to welcome page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import Welcome from './components/Welcome';
+import Email from './components/Email';
 function App() {
   return (
     <div className="App">
-     
+     <Welcome/>
+     <Email/>
     </div>
   );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,38 @@
-import React,{} from 'react'
+import React,{useState} from 'react';
 
 const Welcome = () => {
+
+    const [title,setTitle]=useState('Welcome');
+    const [description,setDescription]=useState('This is the welcome screen');
+    const [image,setImage]=useState(null);
+
+
+    const handleImageUpload = (e)=>{
+        setImage(URL.createObjectURL(e.target.files[0]))
+    };
+
   return (
-    <div>Welcome</div>
+    <div className="welcome-screen">
+    <input
+    type='text'
+    value={title}
+    onChange={(e)=>setTitle(e.target.value)}
+    placeholder='Title'
+    />
+    <textarea
+        value={description}
+         onChange={(e)=>setDescription(e.target.value)}
+    placeholder='Description'
+    />
+     <input
+    type='file'
+    onChange={handleImageUpload}
+    />
+    {image && <img src={image} alt='uploaded' />}
+    <h1>{title}</h1>
+    <p>{description}</p>
+  </div>
   )
 }
 
-export default Welcome
+export default Welcome;
